a063f0a7c4f3cff802a442e70c42549794099c89
Fernando Hnerique Tozetto <fhtozetto@gmail.com>
2020-12-12 20:44:43 -0300

    Password recovery

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -7,7 +7,10 @@ import IAppointmentsRepository from '@modules/appointments/repositories/IAppoint
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
-import UsersRepository from '@modules/users/infra/typeorm/repositories/usersRepositories';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepositories';
+
+// import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+// import UserTokensRepository from '@modules/users/infra/typeorm/repositories/userTokensRepositories';
 
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
